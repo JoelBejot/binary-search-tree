f19a185cce2472724ef43da7e113b4cb3ce1c952
JoelBejot <61334576+JoelBejot@users.noreply.github.com>
2023-03-30 10:44:15 -0600

    refactored code to conform with rubocop linting

--- binary-tree.rb
+++ binary-tree.rb
@@ -1,7 +1,81 @@
-require './node.rb'
+require './node'
+
+# This module condtains all the methods for determining the order of a binary tree.
+module OrderMethods
+  # Returns values in an array in level order. Can take an optional block
+  def level_order(node = root, result = [], &block)
+    return nil if node.nil?
+
+    h = height(node)
+    i = 0
+    while i <= (h + 1)
+      current_level(i, result, node, &block)
+      i += 1
+    end
+    result
+  end
+
+  # Helper method for level_order method
+  def current_level(level, result, node = root, &block)
+    return if node.nil?
+
+    if level == 1
+      result.push(block_given? ? block.call(node) : node.data)
+    elsif level > 1
+      current_level(level - 1, result, node.left, &block)
+      current_level(level - 1, result, node.right, &block)
+    end
+  end
+
+  # Returns values in numerical order, can take optional block
+  def inorder(node = root, result = [], &block)
+    return if node.nil?
+
+    inorder(node.left, result, &block)
+    result.push(block_given? ? block.call(node) : node.data)
+    inorder(node.right, result, &block)
+
+    result
+  end
+
+  # Returns values in pre-order, can take optional block
+  def preorder(node = root, result = [], &block)
+    return if node.nil?
+
+    result.push(block_given? ? block.call(node) : node.data)
+    preorder(node.left, result, &block)
+    preorder(node.right, result, &block)
+
+    result
+  end
+
+  # Returns values in post-order, can take optional block
+  def postorder(node = root, result = [], &block)
+    return if node.nil?
+
+    preorder(node.left, result, &block)
+    preorder(node.right, result, &block)
+    result.push(block_given? ? block.call(node) : node.data)
+
+    result
+  end
+end
+
+# This module contains only the method pretty_print given by the assignment.
+module PrettyPrint
+  # Method given from the project to print the binary tree in an attractive manner
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+end
 
 # Builds the whole tree and contains tree methods
 class Tree
+  include OrderMethods
+  include PrettyPrint
+
   attr_accessor :root
 
   # Builds tree from sorted array of unique numbers
@@ -15,19 +89,18 @@ class Tree
   def build_tree(array)
     return nil if array.empty?
     return Node.new(array[0]) if array.length <= 1
-    
+
     mid = (array.length - 1) / 2
     node = Node.new(array[mid])
 
     node.left = build_tree(array[0...mid])
-    node.right = build_tree(array[mid+1..-1])
-  
+    node.right = build_tree(array[mid + 1..-1])
     node
   end
 
   # Inserts value into tree
   def insert(value, node = @root)
-    return @root = Node.new(value) if @root.nil? 
+    return @root = Node.new(value) if @root.nil?
 
     if value < node.data
       node.left ? insert(value, node.left) : node.left = Node.new(value)
@@ -38,147 +111,88 @@ class Tree
   end
 
   # Returns maximum number in tree
-  def find_max(node = self.root)
+  def find_max(node = root)
     return node if node.nil?
-    if node.right == nil
-      return node
-    end
-    return find_max(node.right)
+    return node if node.right.nil?
+
+    find_max(node.right)
   end
 
   # Returns minimum number in tree
-  def find_min(node = self.root)
+  def find_min(node = root)
     return node if node.nil?
-    if node.left == nil
-      return node
-    end
-    return find_min(node.left)
+    return node if node.left.nil?
+
+    find_min(node.left)
   end
 
   # Removes value from tree and copies children if necessary
-  # three conditions - 
+  # three conditions -
   # 1. Node to be deleted is the leaf - delete the node
   # 2. Node to be deleted has one child - delete the node and copy the child to the node
   # 3. Node to be deleted has two children - delete the node,
-    # find the inorder sucessor, copy the inorder successor to the current node
-  def delete(value, node = self.root)
+  # find the inorder sucessor, copy the inorder successor to the current node
+  def delete(value, node = root)
     return nil if node.nil?
-    pointer = node
 
-    if value < pointer.data
-      pointer.left = delete(value, pointer.left)
-    elsif value > pointer.data
-      pointer.right = delete(value, pointer.right)
+    if value < node.data
+      node.left = delete(value, node.left)
+    elsif value > node.data
+      node.right = delete(value, node.right)
+    elsif node.left && node.right
+      min_of_right_subtree = find_min(node.right)
+      node.data = min_of_right_subtree.data
+      node.right = delete(min_of_right_subtree.data, node.right)
+    elsif !node.left.nil?
+      node = node.left
+    elsif !node.right.nil?
+      node = node.right
     else
-      if pointer.left && pointer.right
-        min_of_right_subtree = find_min(pointer.right)
-        pointer.data = min_of_right_subtree.data
-        pointer.right = delete(min_of_right_subtree.data, pointer.right)
-      elsif pointer.left != nil
-        pointer = pointer.left
-      elsif pointer.right != nil
-        pointer = pointer.right
-      else
-        pointer = nil
-      end
+      node = nil
     end
-    return pointer
+    node
   end
 
   # Returns the node that contains the value in the method call
-  def find(value, node = self.root)
+  def find(value, node = root)
     return nil if node.nil?
+
     if value < node.data
       find(value, node.left)
     elsif value > node.data
       find(value, node.right)
     else
-      return node
+      node
     end
-  end
-
-  # Returns values in an array in level order. Can take an optional block
-  def level_order(node = self.root, result = [], &block)
-    return nil if node.nil?
-
-    h = height(node)
-    i = 0
-    while i <= (h + 1)
-      current_level(node, i, result, &block)
-      i += 1
-    end 
-    result   
-  end
-
-  # Helper method for level_order method
-  def current_level(node = self.root, level, result, &block)
-    return if node.nil?
-
-    if level == 1
-      result.push(block_given? ? block.call(node) : node.data)
-    elsif level > 1
-      current_level(node.left, level - 1, result, &block)
-      current_level(node.right, level - 1, result, &block)
-    end
-  end
-
-  # Returns values in numerical order, can take optional block
-  def inorder(node = self.root, result = [], &block)
-    return if node.nil?
-
-    inorder(node.left, result, &block)
-    result.push(block_given? ? block.call(node) : node.data)
-    inorder(node.right, result, &block)
-
-    result
-  end
-
-  # Returns values in pre-order, can take optional block
-  def preorder(node = self.root, result = [], &block)
-    return if node.nil?
-
-    result.push(block_given? ? block.call(node) : node.data)
-    preorder(node.left, result, &block)
-    preorder(node.right, result, &block)
-
-    result
-  end
-
-  # Returns values in post-order, can take optional block
-  def postorder(node = self.root, result = [], &block)
-    return if node.nil?
-
-    preorder(node.left, result, &block)
-    preorder(node.right, result, &block)
-    result.push(block_given? ? block.call(node) : node.data)
-
-    result
   end
 
   # Returns the height of given node from the farthest leaf node
-  def height(node = self.root, counter = -1)
+  def height(node = root, counter = -1)
     return counter if node.nil?
+
     counter += 1
 
     [height(node.left, counter), height(node.right, counter)].max
   end
 
   # Returns the depth of a value from a node from the root node
-  def depth(value, node = self.root, counter = 0)
+  def depth(value, node = root, counter = 0)
     return nil if node.nil?
 
     if value < node.data
-      depth(value, node.left, counter += 1)
+      counter += 1
+      depth(value, node.left, counter)
     elsif value > node.data
-      depth(value, node.right, counter += 1)
+      counter += 1
+      depth(value, node.right, counter)
     else
-      return counter
+      counter
     end
   end
 
-  # Returns boolean whether or not the tree is balanced. 
+  # Returns boolean whether or not the tree is balanced.
   # I.e., the right sub-tree is not more than one level different than the left sub-tree
-  def balanced?(node = self.root)
+  def balanced?(node = root)
     return true if node.nil?
 
     left = height(@root.left, 0)
@@ -190,12 +204,5 @@ class Tree
   def rebalance
     new_array = inorder.uniq
     @root = build_tree(new_array)
-  end
-
-  # Method given from the project to print the binary tree in an attractive manner
-  def pretty_print(node = @root, prefix = '', is_left = true)
-    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
-    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
-    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
--- main.rb
+++ main.rb
@@ -1,43 +1,54 @@
-require './binary-tree.rb'
-require './node.rb'
+# frozen_string_literal: true
+
+require './binary-tree'
+require './node'
 
 # Driver program that tests the methods of the binary tree
-p "Creating a new Binary Search Tree..."
+p 'Creating a new Binary Search Tree...'
 array = (Array.new(15) { rand(1..100) })
 bst = Tree.new(array)
-puts ""
-p "Is the tree balanced?"
+puts ''
+p 'Is the tree balanced?'
 p bst.balanced?
 
-puts ""
-p "Here they are in order:"
+p 'Find the max number'
+p bst.find_max.data
+max = bst.find_max.data
+p "Let's delete the max number"
+bst.delete(max)
+
+puts ''
+p 'Here they are in order:'
 p "Level order: #{bst.level_order}"
 p "Preorder: #{bst.preorder}"
 p "Postorder: #{bst.postorder}"
 p "Inorder: #{bst.inorder}"
 
-puts ""
+puts ''
 p "Let's add several numbers to the tree..."
-puts ""
-puts ""
+puts ''
+puts ''
 
-15.times do |num| 
+15.times do |num|
   num = rand(101..1000)
-  bst.insert(num) 
+  bst.insert(num)
 end
 
-p "Is the tree still balanced?"
+p 'Is the tree still balanced?'
 p bst.balanced?
-puts ""
+puts ''
 p "Let's rebalance it."
-p "Rebalancing..."
+p 'Rebalancing...'
 bst.rebalance
-puts ""
-p "Now is it balanced?"
+puts ''
+p 'Now is it balanced?'
 p bst.balanced?
-puts ""
-p "Here they are again in order:"
+puts ''
+p 'Here they are again in order:'
 p "Level order: #{bst.level_order}"
 p "Preorder: #{bst.preorder}"
 p "Postorder: #{bst.postorder}"
 p "Inorder: #{bst.inorder}"
+
+p 'And here is the tree in pretty_print:'
+bst.pretty_print
